fc9e2661712ce47c3e4c1b2d12a36240385f0844
sayhanne <sayhannee@gmail.com>
2021-07-27 18:11:23 +0300

    DB url has been secured and DB connection initialized.

--- app/src/main/java/com/example/desty/MainActivity.java
+++ app/src/main/java/com/example/desty/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.desty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -15,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initializeConn();
+    }
+
+    public Connection initializeConn(){
+        Connection connection = null;
+        String conn_url;
+
+        try {
+            conn_url = BuildConfig.DB_URL;
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connection = DriverManager.getConnection(conn_url);
+
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
