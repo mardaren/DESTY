e46ce33f2d87faa2236601c21aa401a3535345fc
Pakiş <ahmet50066@gmail.com>
2021-07-28 23:06:49 +0300

    2 adet query yazdım kullancı ekleme ve kullanıcı girişi için

--- app/src/main/java/com/example/desty/MainActivity.java
+++ app/src/main/java/com/example/desty/MainActivity.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -36,5 +37,31 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return connection;
+    }
+    /* kullanıcı bilgileri doğru mu kontrlü, deneme için ekledim */
+    private boolean profileCheck(Connection conn,String userName,String password) throws SQLException {
+        PreparedStatement ps = conn.prepareStatement("SELECT * FROM [User] WHERE username = ? AND password = ?  ");
+        ps.setObject(1, userName);
+        ps.setObject(2, password);
+        ResultSet rs = ps.executeQuery();
+        if(rs == null)
+            return false ;
+        return true ;
+
+    }
+    /* kullanıcı ekleme */
+    private boolean addUser(Connection conn,String userName,String password) throws SQLException {
+        PreparedStatement ps1 = conn.prepareStatement("SELECT MAX (id) FROM [User] ");
+        ResultSet rs = ps1.executeQuery();
+        int id = rs.getInt(1) + 1;
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO [user] VALUES (?,?,?)");
+        ps.setObject(1, id);
+        ps.setObject(2, userName);
+        ps.setObject(3, password);
+        ResultSet rs1 = ps.executeQuery();
+
+
+        return true ;
+
     }
 }
