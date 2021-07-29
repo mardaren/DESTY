e471261c89a56bc7391b829594f91db310bf919c
mardaren <mardaren1907@gmail.com>
2021-07-29 13:54:54 +0300

    tabs added

--- app/src/main/java/com/example/desty/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/desty/ui/main/SectionsPagerAdapter.java
@@ -8,7 +8,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.desty.HomepageFragment;
+import com.example.desty.ProfileFragment;
 import com.example.desty.R;
+import com.example.desty.SearchFragment;
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -17,7 +20,7 @@ import com.example.desty.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -27,9 +30,19 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new HomepageFragment();
+                break;
+            case 1:
+                fragment = new SearchFragment();
+                break;
+            case 2:
+                fragment = new ProfileFragment();
+                break;
+        }
+        return fragment;
     }
 
     @Nullable
@@ -40,7 +53,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        // Show 3 total pages.
+        return 3;
     }
 }
