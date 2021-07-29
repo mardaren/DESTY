1c7c74318a1250bc63bea0a6926f7832ba3a510d
mardaren <mardaren1907@gmail.com>
2021-07-29 13:04:57 +0300

    Main activity fix I

--- app/src/main/java/com/example/desty/MainActivity.java
+++ app/src/main/java/com/example/desty/MainActivity.java
@@ -1,26 +1,51 @@
 package com.example.desty;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import java.io.IOException;
-import java.io.InputStream;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.desty.ui.main.SectionsPagerAdapter;
+import com.example.desty.databinding.ActivityMainBinding;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //initializeConn();
-    }
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
     public Connection initializeConn(){
         Connection connection = null;
         String conn_url;
---
+++ app/src/main/java/com/example/desty/ScreenLoading.java
@@ -0,0 +1,5 @@
+package com.example.desty;
+
+public class ScreenLoading {
+
+}
