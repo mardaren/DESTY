161436a156b39b8a7498562e13ef7d0d9051c0ee
sayhanne <sayhannee@gmail.com>
2021-08-12 14:18:14 +0300

    Home page button for map is added. Dependencies for Maps SDK has been added.

--- app/src/main/java/com/example/desty/HomepageFragment.java
+++ app/src/main/java/com/example/desty/HomepageFragment.java
@@ -1,9 +1,11 @@
 package com.example.desty;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +14,22 @@ import androidx.fragment.app.Fragment;
 import org.jetbrains.annotations.NotNull;
 
 public class HomepageFragment extends Fragment {
+    Button mapButton;
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_homepage, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapButton = view.findViewById(R.id.map_button);
+        mapButton.setOnClickListener(v -> {
+            Intent i = new Intent(getActivity(), MapsActivity.class);
+            startActivity(i);
+            getActivity().finish();
+        });
     }
 }
