1da6dc7f11e4193fd56f9ec26a3f75f1440de31d
mardaren <mardaren1907@gmail.com>
2021-08-13 13:39:01 +0300

    search and profile tab activities added

---
+++ app/src/main/java/com/example/desty/profile/RoutesActivity.java
@@ -0,0 +1,16 @@
+package com.example.desty.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.desty.R;
+
+public class RoutesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routes);
+    }
+}
